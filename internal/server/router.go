c0aa172c4b34ae311a284e62f86fe0f7a6f1b2bd
axolotlmaid <hello@axolotlmaid.com>
2024-09-25 22:36:12 +0100

    fix: add cors

--- internal/server/router.go
+++ internal/server/router.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/go-chi/httprate"
 
 	"backend/internal/handler"
@@ -23,6 +24,9 @@ func NewRouter() {
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Timeout(60 * time.Second))
 	r.Use(httprate.LimitByRealIP(32, time.Minute))
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://axolotlmaid.com"},
+	}))
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "https://axolotlmaid.com", http.StatusPermanentRedirect)
