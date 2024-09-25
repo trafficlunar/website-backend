cb61a2a8b7251e6917a26060d02a67434435b7b5
axolotlmaid <hello@axolotlmaid.com>
2024-09-25 16:52:29 +0100

    feat: add .env

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -4,10 +4,18 @@ import (
 	"log/slog"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/lmittmann/tint"
 )
 
 func main() {
 	logger := slog.New(tint.NewHandler(os.Stderr, nil))
 	slog.SetDefault(logger)
+
+	err := godotenv.Load()
+	if err != nil {
+		slog.Error("Error loading .env file", slog.Any("error", err))
+	}
+
+	slog.Info("Starting server", slog.Any("port", os.Getenv("PORT")))
 }
